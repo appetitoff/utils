e7b3a224ed3018b8832b19cd0d3db98e353dc19b
SecorD0 <25284138+SecorD0@users.noreply.github.com>
2022-09-18 18:20:31 +0300

    Installs/uninstalls NVM

--- installers/nvm.sh
+++ installers/nvm.sh
@@ -24,7 +24,8 @@ while test $# -gt 0; do
 		echo
 		echo -e "${C_LGn}Useful URLs${RES}:"
 		echo -e "https://github.com/SecorD0/utils/blob/main/installers/nvm.sh - script URL"
-		echo -e "https://t.me/letskynode — node Community"
+		echo -e "https://t.me/OnePackage — noderun and tech community"
+		echo -e "https://learning.1package.io — guides and articles"
 		echo -e "https://teletype.in/@letskynode — guides and articles"
 		echo
 		return 0 2>/dev/null; exit 0
