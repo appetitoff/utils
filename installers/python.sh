7c908ac09a4d4fc6fd00ea85d4d9be0631c97523
SecorD0 <25284138+SecorD0@users.noreply.github.com>
2022-09-18 18:20:57 +0300

    Installs Python

--- installers/python.sh
+++ installers/python.sh
@@ -23,7 +23,8 @@ while test $# -gt 0; do
 		echo
 		echo -e "${C_LGn}Useful URLs${RES}:"
 		echo -e "https://github.com/SecorD0/utils/blob/main/installers/python.sh - script URL"
-		echo -e "https://t.me/letskynode — node Community"
+		echo -e "https://t.me/OnePackage — noderun and tech community"
+		echo -e "https://learning.1package.io — guides and articles"
 		echo -e "https://teletype.in/@letskynode — guides and articles"
 		echo
 		return 0 2>/dev/null; exit 0
